da2b29696c352d31f8e8b77229532dea20476e28
MdAzka <muhammadazka6160@gmail.com>
2025-11-26 20:22:37 +0700

    feat: add stock alert feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:warung_ku/hello_world_page.dart';
+import 'package:warung_ku/features/stock_alert/views/pages/stock_alert_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,16 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const HelloWorldPage(),
-    );
+    return MaterialApp(home: const StockAlertPage());
   }
 }
